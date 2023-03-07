206877c028241382eb7d344d4f8cf414c080c864
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-03-07 11:48:26 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from tkinter import*
+import random
+
+def next_turn():
+    pass
+
+def check_winner():
+    pass
+
+def new_game():
+    pass
